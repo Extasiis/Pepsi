f3e0a12d8d61966dc17e4b465dbe5a71f800c9ec
Davidcode <dm22657@gmail.com.com>
2022-07-18 22:29:53 -0400

    responsive

--- js/index.js
+++ js/index.js
@@ -6,3 +6,10 @@ function changeBgColor(color){
     const sec = document.querySelector('.sec');
     sec.style.background = color;
 }
+
+function menuToggle(){
+    const toggleMenu = document.querySelector('.toggleMenu');
+    const navegation = document.querySelector('.navegation');
+    toggleMenu.classList.toggle('active');
+    navegation.classList.toggle('active');
+}
